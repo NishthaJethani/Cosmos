7baecfb1000e3cc874b741cd01c12661d88bd0a6
Nishtha <95950571+NishthaJethani@users.noreply.github.com>
2023-04-23 19:38:35 +0530

    Delete backend directory

--- backend/leavemanagement/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns=[
-    path("student/register", views.register_student, name="register_student"),
-    path("home", views.home, name='home'),
-]
